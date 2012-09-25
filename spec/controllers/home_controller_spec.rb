c0bd4d9e69e1816460f87228056685d4912ae3b3
Vitaliy Beskrovny <bva@aejis.eu>
2012-09-25 19:10:15 +0300

    + home_controller tests

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe HomeController do
+
+  context "unauthorized user" do
+
+    it "redirects to money_operations" do
+      get :index
+      response.should_not redirect_to(money_operations_path)
+    end
+  end
+
+  context "unauthorized user" do
+    before :each do
+      authenticate_user
+    end
+
+
+    it "redirects to money_operations" do
+      get :index
+      response.should redirect_to(money_operations_path)
+    end
+  end
+end
