ca196098152c9c3df30a02ddca00779b4fa72358
Vitaliy Beskrovny <bva@aejis.eu>
2012-10-08 02:17:54 +0300

    + javascript for families handling

---
+++ app/assets/javascripts/families.js
@@ -0,0 +1,36 @@
+MB.families = {
+  update: function(link_tag) {
+    var action_url = $(link_tag).data("url"),
+      input_object = $(link_tag).parent().children("input"),
+      field_name = "name",
+      user_data = input_object.val(),
+      data_string = '{ "family": { "' + field_name + '": "' + user_data + '"} }',
+      message_class;
+
+    data_string = jQuery.parseJSON(data_string);
+
+    $.ajax({
+      type: 'PUT',
+      url: action_url,
+      dataType: 'json',
+      data: data_string
+    }).done(function(message){
+
+        if(message.status == 200) {
+          message_class = "notice";
+          $("h1#family_name_header").text(user_data);
+        }else{ message_class = "error" }
+
+        MB.messages.new(message_class, message.result);
+      });
+
+  }
+};
+
+$(function() {
+  $("a#change_family_name").live("click", function(e) {
+    MB.families.update(this);
+    e.preventDefault();
+  });
+
+});
