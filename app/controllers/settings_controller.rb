e89f893271abcf5858d659347effe1dd93a4e711
Vitaliy Beskrovny <bva@aejis.eu>
2012-10-08 00:47:36 +0300

    * separate invite lists on "Invite me" and "Invite by me"

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,9 +7,11 @@ class SettingsController < ApplicationController
   def index
     authorize! :view, Family
 
-    @user_list = current_user.relatives(false)
-    @my_invites = current_user.invites
-    @invited_by_family = current_user.family.invites
+    #@user_list = current_user.relatives(false)
+    @my_invites = { from_me: current_user.invites_from_me,
+                    to_me:  current_user.invites_to_me }
+    @family_invites = { from_family: current_user.family.invites_from_family,
+                        to_family:  current_user.family.invites_to_family }
   end
 
 end
