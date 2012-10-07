00243a1383504785179fbcc197274a80346c4656
Vitaliy Beskrovny <bva@aejis.eu>
2012-10-08 00:46:55 +0300

    * loading and authorizing :create and :reject methods manually; * bug fixes

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,15 +1,52 @@
 # encoding: utf-8
 class InvitesController < ApplicationController
 
-  load_and_authorize_resource
+  load_and_authorize_resource     except: [:create, :reject]
 
   ##
   # Create new invite
+  # Method for AJAX callback
   #
   def create
-    if @invite.save!
+    invite = nil
+    # is this invite from user to family
+    if params[:invite][:family_name].present?
+      authorize! :create, Invite
+      new_family = Family.find_by_name(params[:invite][:family_name])
+      if new_family.present?
+        invite = Invite.new(family: new_family,
+                            user: current_user,
+                            creator: current_user,
+                            is_sent_to_email: false)
+      else
+        respond_to do |format|
+          format.js { render json: { result: "Family not found", status: 404 } and return }
+        end
+      end
+
+    # is this invite from family to user
+    elsif params[:invite][:user_data].present?
+      authorize! :create, Invite
+      # check if this user exists in database
+      user = User.find_by_login(params[:invite][:user_data]) || User.find_by_email(params[:invite][:user_data])
+      if user.present?
+        invite = Invite.new(family: current_user.family,
+                            user: user,
+                            creator: current_user,
+                            is_sent_to_email: false)
+      else
+        #TODO: send the email with invite to user
+        respond_to do |format|
+          format.js { render json: {result: "Email is sent", status: 204 } and return }
+        end
+      end
+
+    end
+
+    if invite && invite.save!
       respond_to do |format|
-        format.js { render json: {result: "ok", status: 201 } }
+        render_string = render_to_string(partial: "invites/invite", layout: false, locals: {name: invite.name_for_family, invite_id: invite.id, can_manage: false})
+        format.js { render json: { result: render_string, status: 201 }}
       end
     else
       respond_to do |format|
@@ -20,6 +57,7 @@ class InvitesController < ApplicationController
 
   ##
   # Accept invite
+  # Method for AJAX callback
   #
   def accept
     user = User.find_by_id(@invite.user_id).first
@@ -47,9 +85,12 @@ class InvitesController < ApplicationController
 
   ##
   # Reject invite
+  # Method for AJAX callback
   #
   def reject
+    @invite = Invite.find_by_id(params[:id])
     if @invite
+      authorize! :reject, @invite
       if @invite.delete
         respond_to do |format|
           format.js { render json: {result: "ok", status: 200 } }
@@ -59,8 +100,9 @@ class InvitesController < ApplicationController
           format.js { render json: {result: "failed", status: 500 } }
         end
       end
+    else
       respond_to do |format|
-        format.js { render json: {result: "failed", status: 404 } }
+        format.js { render json: {result: "Invite not found", status: 404 } }
       end
     end
   end
