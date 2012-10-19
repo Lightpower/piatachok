120032a91af8c4a179ad9277f477599c35b6275b
Vitaliy Beskrovny <bva@aejis.eu>
2012-10-19 03:58:17 +0300

    + database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/autorun'
 require "devise/test_helpers"
 require 'capybara/rspec'
 require 'capybara/rails'
+require 'database_cleaner'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
