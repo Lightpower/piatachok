2d52de83fbfd55078e8fc0a961d0a2516d676a32
Vitaliy Beskrovny <bva@aejis.eu>
2012-11-10 17:57:18 +0200

    + family_id to categories

--- db/migrate/20120827193727_create_categories.rb
+++ db/migrate/20120827193727_create_categories.rb
@@ -1,11 +1,13 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :name
-      t.string :type
+      t.string  :name
+      t.integer :family_id
+      t.string  :type
     end
 
     add_index :categories, :name
     add_index :categories, :type
+    add_index :categories, :family_id
   end
 end
