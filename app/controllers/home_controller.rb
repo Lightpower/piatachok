f0916d826be4e8d524d0c5e0cd7c87ae57d99e16
Vitaliy Beskrovny <bva@aejis.eu>
2012-11-12 02:01:23 +0200

    * don't redirect to SpendOperation list if alert is present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    redirect_to money_operations_path if current_user.present?
+    redirect_to money_operations_path if current_user.present? && flash[:alert].blank?
   end
 end
