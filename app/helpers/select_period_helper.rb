274fb072aba0ae9350c24a77467c7918342d9c2b
Vitaliy Beskrovny <bva@aejis.eu>
2013-01-02 18:49:44 +0200

    * improve block style

--- app/helpers/select_period_helper.rb
+++ app/helpers/select_period_helper.rb
@@ -4,9 +4,18 @@ module SelectPeriodHelper
   ##
   #  Combobox for period selecting
   #
-  def select_period_tag(select_period, day)
-    return_string = select_tag(:select_period, options_from_collection_for_select([["День", :day], ["Месяц", :month], ["Год", :year], ["Другой период...", :other]], :last, :first, select_period))
-    return_string << ("<input type=\"text\" id=\"day\" name=\"day\" class=\"datepicker\" disabled=\"true\" readonly=\"true\" value=\"#{day.strftime("%d.%m.%Y")}\">").html_safe
+  def select_period_tag(select_period, day, caption)
+    return_string = content_tag(:span, caption + " ")
+    return_string << select_tag(:select_period, options_from_collection_for_select([["День", :day], ["Месяц", :month], ["Год", :year], ["Другой период...", :other]], :last, :first, select_period))
+    return_string = content_tag(:div, return_string)
+
+    period_string = content_tag(:span, "с ")
+    period_string << ("<input type=\"text\" id=\"day\" name=\"day\" class=\"datepicker\" disabled=\"true\" readonly=\"true\" value=\"#{day.strftime("%d.%m.%Y")}\">").html_safe
+    period_string << content_tag(:span, " по ")
+    period_string << ("<input type=\"text\" id=\"today\" name=\"today\" class=\"datepicker\" disabled=\"true\" readonly=\"true\" value=\"#{day.strftime("%d.%m.%Y")}\">").html_safe
+    period_string = content_tag(:div, period_string)
+
+    return_string << period_string
     return_string.html_safe
   end
 end
