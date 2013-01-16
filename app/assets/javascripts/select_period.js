7b4adce37b6c7589adaea1f8d465c44349ea2b82
Vitaliy Beskrovny <bva@aejis.eu>
2013-01-16 19:21:49 +0200

    * update js for date - end of period

--- app/assets/javascripts/select_period.js
+++ app/assets/javascripts/select_period.js
@@ -3,23 +3,25 @@ $(function() {
   $(".datepicker").datepicker({ dateFormat: "dd.mm.yy" });
 
   $("select#select_period").change(function() {
+    var now = new Date();
     switch($(this).val()) {
       case "day":
-        $("input#day[type=text]").attr("disabled", true);
-        $("input#day[type=text]").val($.datepicker.formatDate('dd.mm.yy', new Date()));
+        $("input.datepicker").attr("disabled", true);
+        $("input.datepicker").val($.datepicker.formatDate('dd.mm.yy', new Date()));
+        $("input#day2").val($.datepicker.formatDate('dd.mm.yy', new Date()));
         break;
       case "month":
-        $("input#day[type=text]").attr("disabled", true);
-        var now = new Date();
-        $("input#day[type=text]").val($.datepicker.formatDate('dd.mm.yy', new Date(now.getFullYear(), now.getMonth(), 1)));
+        $("input.datepicker").attr("disabled", true);
+        $("input.datepicker").val($.datepicker.formatDate('dd.mm.yy', new Date(now.getFullYear(), now.getMonth(), 1)));
+        $("input#day2").val($.datepicker.formatDate('dd.mm.yy', new Date()));
         break;
       case "year":
-        $("input#day[type=text]").attr("disabled", true);
-        var now = new Date();
-        $("input#day[type=text]").val($.datepicker.formatDate('dd.mm.yy', new Date(now.getFullYear(), 0, 1)));
+        $("input.datepicker").attr("disabled", true);
+        $("input.datepicker").val($.datepicker.formatDate('dd.mm.yy', new Date(now.getFullYear(), 0, 1)));
+        $("input#day2").val($.datepicker.formatDate('dd.mm.yy', new Date()));
         break;
       case "other":
-        $("input#day[type=text]").removeAttr("disabled");
+        $("input.datepicker").removeAttr("disabled");
         break;
     }
   });
