5e9d52cf8e5e6cb8f4d4a79c2ca780f2312b8c20
Vitaliy Beskrovny <bva@aejis.eu>
2013-01-28 15:48:09 +0200

    * select period disabling fixed

--- app/helpers/select_period_helper.rb
+++ app/helpers/select_period_helper.rb
@@ -9,11 +9,13 @@ module SelectPeriodHelper
     return_string << select_tag(:select_period, options_from_collection_for_select([["За день", :day], ["За месяц", :month], ["За год", :year], ["Другой период...", :other]], :last, :first, period_type))
     return_string = content_tag(:div, return_string)
 
+    disabled = ""
+    disabled = "disabled=\"true\"" if period_type != "other"
     period_string = content_tag(:span, "с ")
-    period_string << ("<input type=\"text\" id=\"day\" name=\"day\" class=\"datepicker\" disabled=\"true\" readonly=\"true\" value=\"#{period.first.strftime("%d.%m.%Y")}\">").html_safe
+    period_string << ("<input type=\"text\" id=\"day\" name=\"day\" class=\"datepicker\"#{disabled} readonly=\"true\" value=\"#{period.first.strftime("%d.%m.%Y")}\">").html_safe
     if change_last_date
       period_string << content_tag(:span, " по ")
-      period_string << ("<input type=\"text\" id=\"day2\" name=\"day2\" class=\"datepicker\" disabled=\"true\" readonly=\"true\" value=\"#{period.last.strftime("%d.%m.%Y")}\">").html_safe
+      period_string << ("<input type=\"text\" id=\"day2\" name=\"day2\" class=\"datepicker\"#{disabled} readonly=\"true\" value=\"#{period.last.strftime("%d.%m.%Y")}\">").html_safe
     else
       period_string << content_tag(:span, " по сегодня")
     end
